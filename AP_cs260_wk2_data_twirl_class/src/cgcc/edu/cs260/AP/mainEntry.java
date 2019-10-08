061a8a71f8adbb9210de78c0eeedaf9191b20216
adam <55722970+adampentz@users.noreply.github.com>
2019-10-07 22:08:48 -0700

    inClass revisions

---
+++ AP_cs260_wk2_data_twirl_class/src/cgcc/edu/cs260/AP/mainEntry.java
@@ -0,0 +1,12 @@
+package cgcc.edu.cs260.AP;
+
+public class mainEntry {
+
+	public static void main(String[] args) {
+		//create and fill 2D array to print image
+		action arr = new action();
+		arr.printArray();
+
+	}
+
+}
